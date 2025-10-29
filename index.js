ae02066c8962bcfabad332ac00723c736974f89b
jjacque1 <jacksonjacque.jj@gmail.com>
2025-10-29 14:04:02 -0400

    refactor html as a form and included labes for each input field

--- index.js
+++ index.js
@@ -1,40 +1,68 @@
 //Getting the html input values by id
 
-let categoryInput = document.getElementById("categoryInput");
+let categoryInput = document.getElementById("categoryInput"); 
 
 let taskInput = document.getElementById("taskInput");
 
 let deadlineInput = document.getElementById("deadlineInput");
 
 let taskList = document.getElementById("taskList");
-let categoryList = document.getElementById("categoryList")
-
+let categoryList = document.getElementById("categoryList");
+let dueDate = document.getElementById("dueDate");
 let statusUpdate = document.getElementById("statusUpdate");
 
 addTaskButton.addEventListener("click", function () {
-    let taskText = taskInput.value;
-    if (taskText === "") {
-        alert("Please enter a task to get started!")
-        return;
-    }
+  let taskText = taskInput.value;
+  if (taskText === "") {
+    alert("Please enter a task to get started!");
+    return;
+  }
 
-    let listItem = document.createElement("li");
-    listItem.innerText = taskText
+  let listItem = document.createElement("li");
+  listItem.innerText = taskText;
 
-    taskList.append(listItem);
-    taskInput.value = "";
-})
+  taskList.append(listItem);
+  taskInput.value = "";
+});
 
 addTaskButton.addEventListener("click", function () {
-    let categoryText = categoryInput.value;
-    if (categoryText === "") {
-        alert("Please add a Task Category.")
-        return;
-    }
+  let categoryText = categoryInput.value;
+  if (categoryText === "") {
+    alert("Please add a Task Category.");
+    return;
+  }
 
-    let listItem = document.createElement("li");
-    listItem.innerText = categoryText;
+  let listItem = document.createElement("li");
+  listItem.innerText = categoryText;
 
-    categoryList.append(listItem);
-    categoryInput.value = ""
-})
+  categoryList.append(listItem);
+  categoryInput.value = "";
+});
+
+addTaskButton.addEventListener("click", function () {
+  let categoryText = categoryInput.value;
+  if (categoryText === "") {
+    alert("Please add a Task Category.");
+    return;
+  }
+
+  let listItem = document.createElement("li");
+  listItem.innerText = categoryText;
+
+  categoryList.append(listItem);
+  categoryInput.value = "";
+});
+
+addTaskButton.addEventListener("click", function () {
+  let categoryText = categoryInput.value;
+  if (categoryText === "") {
+    alert("Please add a Task Category.");
+    return;
+  }
+
+  let listItem = document.createElement("li");
+  listItem.innerText = categoryText;
+
+  categoryList.append(listItem);
+  categoryInput.value = "";
+});
